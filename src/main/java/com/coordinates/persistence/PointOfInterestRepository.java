3f5c6f9662c4b9dd98c3e4f38d27b534d0372e2e
Thalles Montenegro <thalles.cezar.am@gmail.com>
2024-06-24 11:37:32 -0300

    Adding jpa repository

---
+++ src/main/java/com/coordinates/persistence/PointOfInterestRepository.java
@@ -0,0 +1,9 @@
+package com.coordinates.persistence;
+
+import com.coordinates.model.PointOfInterest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PointOfInterestRepository extends JpaRepository<PointOfInterest, Long> {
+}
